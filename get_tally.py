7d59ddbeffce2a964896e407c32f399f1a25bcae
Pascal Weber <ppunktw@gmail.com>
2020-09-26 02:38:26 +0200

    adding poll countdown

--- get_tally.py
+++ get_tally.py
@@ -1,6 +1,7 @@
 import requests
 import json
-from config_private import TEL_URL, DISCORD_HOOK_ID, DISCORD_HOOK_TOKEN
+import datetime
+from config_private import TEL_URL, DISCORD_HOOK_ID, DISCORD_HOOK_TOKEN, ETHERSCAN_KEY
 from discord import Webhook, RequestsWebhookAdapter
 
 def get_totalActiveStake():
@@ -15,7 +16,13 @@ def get_totalActiveStake():
     activeStake = round(int(r.json()["data"]["protocols"][0]["totalActiveStake"])/10**18)
     return activeStake
 
-def get_tally(poll, title, numberVoted):
+def get_countdown(endBlock):
+    ETHERSCAN_API = f"https://api.etherscan.io/api?module=block&action=getblockcountdown&blockno={endBlock}&apikey={ETHERSCAN_KEY}"
+    countdown = requests.get(ETHERSCAN_API).json()
+    secs = int(countdown["result"]["EstimateTimeInSec"].split(".")[0])
+    return str(datetime.timedelta(seconds=secs))
+
+def get_tally(poll):
     GRAPH_URL = 'https://api.thegraph.com/subgraphs/name/livepeer/livepeer'
 
     query = """query {
@@ -32,12 +39,19 @@ def get_tally(poll, title, numberVoted):
     votes_n = round(int(votes["no"])/10**18)
     votes_t = votes_y + votes_n
     activeStake = get_totalActiveStake()
+    with open("active_polls.json", "r") as f:
+        polls = json.load(f)
+    title = polls[poll]["title"]
+    numberVoted = len(polls[poll]["voted"])
+    endBlock = polls[poll]["endBlock"]
+    countdown = get_countdown(endBlock)
     message = f"[{title}](https://explorer.livepeer.org/voting/{poll})\n" \
               f"```\n" \
               f"{'Yes:':>4} {str(round(votes_y/votes_t*100,2))+'%':>5} {votes_y:>10,} LPT\n" \
               f"{'No:':>4}  {str(round(votes_n/votes_t*100,2))+'%':>5} {votes_n:>10,} LPT\n\n" \
               f"Participation: {round(votes_t/activeStake*100,2)}%\n" \
-              f"{numberVoted} Orchestrators voted\n" \
+              f"{numberVoted} Orchestrators voted\n\n" \
+              f"Poll ends in approx. {countdown}!\n" \
               f"```"
     return message
 
@@ -62,8 +76,6 @@ if __name__ == "__main__":
     with open("active_polls.json", "r") as f:
         polls = json.load(f)
     for poll in polls.copy():
-        title = polls[poll]["title"]
-        numberVoted = len(polls[poll]["voted"])
-        message = get_tally(poll, title, numberVoted)
+        message = get_tally(poll)
         send_telegram(message, "@LivepeerGovernance")
         send_discord(message)
